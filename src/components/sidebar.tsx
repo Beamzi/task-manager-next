cf10e891e5d14c9cd4a9b85f29360fd9518818bb
James Day <159420270+Beamzi@users.noreply.github.com>
2025-04-03 09:43:34 +1000

    setup

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+  return (
+    <section className="sideBar flex flex-col">
+      <button className="">Create Task</button>
+      <button className="">All Tasks</button>
+      <button className="">favorites</button>
+    </section>
+  );
+}
